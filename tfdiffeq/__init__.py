80fb8a08ae5853b2aaa665d1f95cebe4ff2daf45
Somshubra Majumdar <titu1994@gmail.com>
2019-02-15 12:21:04 -0600

    Cleanup package namespace

--- tfdiffeq/__init__.py
+++ tfdiffeq/__init__.py
@@ -6,4 +6,9 @@ from tfdiffeq.misc import move_to_device
 from tfdiffeq.viz_utils import plot_phase_portrait, plot_vector_field
 
 
+__all__ = ['odeint',
+           'cast_double', 'func_cast_double', 'move_to_device',
+           'plot_phase_portrait', 'plot_vector_field']
+
+
 __version__ = '0.0.1'
