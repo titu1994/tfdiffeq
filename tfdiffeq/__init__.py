1ae120116713ed06e82ce01dd0bda07f668f261f
Somshubra Majumdar <titu1994@gmail.com>
2019-02-15 10:00:50 -0600

    Correct import

--- tfdiffeq/__init__.py
+++ tfdiffeq/__init__.py
@@ -3,7 +3,7 @@ from tfdiffeq.odeint import odeint
 
 from tfdiffeq.misc import cast_double, func_cast_double
 from tfdiffeq.misc import move_to_device
-from tfdiffeq.viz_utils import plot_phase_plot, plot_vector_field
+from tfdiffeq.viz_utils import plot_phase_portrait, plot_vector_field
 
 
 __version__ = '0.0.1'
