d6406b511608cb0f7fe8a31896c7df7fe488c1be
Somshubra Majumdar <titu1994@gmail.com>
2019-03-24 12:09:43 -0500

    Revert addition of ODEModel

--- tfdiffeq/__init__.py
+++ tfdiffeq/__init__.py
@@ -2,9 +2,6 @@
 from tfdiffeq.odeint import odeint
 # from tfdiffeq.adjoint import odeint_adjoint
 
-# Model imports
-from tfdiffeq.model import ODEModel
-
 # Utility functions
 from tfdiffeq.misc import cast_double, func_cast_double
 from tfdiffeq.misc import move_to_device
@@ -16,10 +13,9 @@ from tfdiffeq.viz_utils import (plot_phase_portrait,
 
 
 __all__ = ['odeint',
-           'ODEModel',
            'cast_double', 'func_cast_double', 'move_to_device',
            'plot_phase_portrait', 'plot_vector_field', 'plot_results',
            ]
 
 
-__version__ = '0.0.0.9.0'
+__version__ = '0.0.1'
