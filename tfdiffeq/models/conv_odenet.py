7e33b05227e81a62f68925d2c7176cfddbaeff73
Somshubra Majumdar <titu1994@gmail.com>
2019-05-27 14:23:29 -0500

    Add tfdiffeq.models to manage ODENet and Augmented ODENet models

---
+++ tfdiffeq/models/conv_odenet.py
@@ -0,0 +1,178 @@
+"""
+Definition of Convolutional ODENet and Augmented ODENet.
+Ported from : https://github.com/EmilienDupont/augmented-neural-odes/blob/master/anode/conv_models.py
+"""
+import tensorflow as tf
+
+from tfdiffeq.models.dense_odenet import ODEBlock
+
+MAX_NUM_STEPS = 1000  # Maximum number of steps for ODE solver
+
+
+class Conv2dTime(tf.keras.Model):
+    """
+    Implements time dependent 2d convolutions, by appending the time variable as
+    an extra channel.
+    """
+    def __init__(self, dim_in, dim_out, kernel_size=3, stride=1, padding=0, dilation=1,
+                 bias=True, transpose=False):
+        super(Conv2dTime, self).__init__()
+        module = tf.keras.layers.Conv2DTranspose if transpose else tf.keras.layers.Conv2D
+
+        padding = 'same'if padding == 1 else 'valid'
+        self._layer = module(
+            dim_out, kernel_size=(kernel_size, kernel_size), strides=(stride, stride), padding=padding,
+            dilation_rate=dilation,
+            use_bias=bias
+        )
+
+        self.channel_axis = 1 if tf.keras.backend.image_data_format() == 'channels_first' else -1
+
+    def call(self, t, x, training=None, **kwargs):
+        # t = tf.cast(t, tf.float32)
+        if self.channel_axis == 1:
+            # Shape (batch_size, 1, height, width)
+            tt = tf.ones_like(x[:, :1, :, :]) * t  # channel dim = 1
+
+        else:
+            # Shape (batch_size, height, width, 1)
+            tt = tf.ones_like(x[:, :, :, :1]) * t  # channel dim = -1
+
+        ttx = tf.concat([tt, x], axis=self.channel_axis)  # concat at channel dim
+
+        return self._layer(ttx)
+
+
+class ConvODEFunc(tf.keras.Model):
+
+    def __init__(self, img_size, num_filters, augment_dim=0,
+                 time_dependent=False, non_linearity='relu', **kwargs):
+        """
+        Convolutional block modeling the derivative of ODE system.
+
+        # Arguments:
+            img_size : tuple of ints
+                Tuple of (channels, height, width).
+            num_filters : int
+                Number of convolutional filters.
+            augment_dim: int
+                Number of augmentation channels to add. If 0 does not augment ODE.
+            time_dependent : bool
+                If True adds time as input, making ODE time dependent.
+            non_linearity : string
+                One of 'relu' and 'softplus'
+        """
+        super(ConvODEFunc, self).__init__(**kwargs)
+        self.augment_dim = augment_dim
+        self.img_size = img_size
+        self.time_dependent = time_dependent
+        self.nfe = 0  # Number of function evaluations
+        self.channels, self.height, self.width = img_size
+        self.channels += augment_dim
+        self.num_filters = num_filters
+
+        if time_dependent:
+            self.conv1 = Conv2dTime(self.channels, self.num_filters,
+                                    kernel_size=1, stride=1, padding=0)
+            self.conv2 = Conv2dTime(self.num_filters, self.num_filters,
+                                    kernel_size=3, stride=1, padding=1)
+            self.conv3 = Conv2dTime(self.num_filters, self.channels,
+                                    kernel_size=1, stride=1, padding=0)
+        else:
+            self.conv1 = tf.keras.layers.Conv2D(self.channels, self.num_filters,
+                                                kernel_size=(1, 1), strides=(1, 1),
+                                                padding='valid')
+            self.conv2 = tf.keras.layers.Conv2D(self.num_filters, self.num_filters,
+                                                kernel_size=(3, 3), strides=(1, 1),
+                                                padding='same')
+            self.conv3 = tf.keras.layers.Conv2D(self.num_filters, self.channels,
+                                                kernel_size=(1, 1), strides=(1, 1),
+                                                padding='valid')
+
+        if non_linearity == 'relu':
+            self.non_linearity = tf.keras.layers.ReLU()
+        elif non_linearity == 'softplus':
+            self.non_linearity = tf.keras.layers.Activation('softplus')
+
+    def call(self, t, x, training=None, **kwargs):
+        """
+        Parameters
+        ----------
+        t : Tensor
+            Current time.
+        x : Tensor
+            Shape (batch_size, input_dim)
+        """
+        self.nfe += 1
+
+        if self.time_dependent:
+            out = self.conv1(t, x)
+            out = self.non_linearity(out)
+            out = self.conv2(t, out)
+            out = self.non_linearity(out)
+            out = self.conv3(t, out)
+        else:
+            out = self.conv1(x)
+            out = self.non_linearity(out)
+            out = self.conv2(out)
+            out = self.non_linearity(out)
+            out = self.conv3(out)
+
+        return out
+
+
+class ConvODENet(tf.keras.Model):
+    """Creates an ODEBlock with a convolutional ODEFunc followed by a Linear
+    layer.
+    Parameters
+    ----------
+    img_size : tuple of ints
+        Tuple of (channels, height, width).
+    num_filters : int
+        Number of convolutional filters.
+    output_dim : int
+        Dimension of output after hidden layer. Should be 1 for regression or
+        num_classes for classification.
+    augment_dim: int
+        Number of augmentation channels to add. If 0 does not augment ODE.
+    time_dependent : bool
+        If True adds time as input, making ODE time dependent.
+    non_linearity : string
+        One of 'relu' and 'softplus'
+    tol : float
+        Error tolerance.
+    adjoint : bool
+        If True calculates gradient with adjoint method, otherwise
+        backpropagates directly through operations of ODE solver.
+    solver: ODE solver. Defaults to DOPRI5.
+    """
+    def __init__(self, img_size, num_filters, output_dim=1,
+                 augment_dim=0, time_dependent=False, non_linearity='relu',
+                 tol=1e-3, adjoint=False, solver='dopri5', **kwargs):
+        super(ConvODENet, self).__init__(**kwargs)
+        self.img_size = img_size
+        self.num_filters = num_filters
+        self.augment_dim = augment_dim
+        self.output_dim = output_dim
+        self.flattened_dim = (img_size[0] + augment_dim) * img_size[1] * img_size[2]
+        self.time_dependent = time_dependent
+        self.tol = tol
+        self.solver = solver
+
+        odefunc = ConvODEFunc(img_size, num_filters, augment_dim,
+                              time_dependent, non_linearity)
+
+        self.odeblock = ODEBlock(odefunc, is_conv=True, tol=tol,
+                                 adjoint=adjoint, solver=solver)
+
+        self.linear_layer = tf.keras.layers.Dense(self.output_dim)
+
+    def call(self, x, training=None, return_features=False):
+        features = self.odeblock(x, training=training)
+        features = tf.reshape([features.shape[0], -1])
+
+        pred = self.linear_layer(features)
+
+        if return_features:
+            return features, pred
+        return pred
---
+++ tfdiffeq/models/dense_odenet.py
@@ -0,0 +1,239 @@
+"""
+Definition of ODENet and Augmented ODENet.
+Ported from https://github.com/EmilienDupont/augmented-neural-odes/blob/master/anode/models.py
+"""
+import tensorflow as tf
+from tfdiffeq import odeint
+
+MAX_NUM_STEPS = 1000  # Maximum number of steps for ODE solver
+
+
+class ODEFunc(tf.keras.Model):
+
+    def __init__(self, data_dim, hidden_dim, augment_dim=0,
+                 time_dependent=False, non_linearity='relu',
+                 **kwargs):
+        """
+        MLP modeling the derivative of ODE system.
+
+        # Arguments:
+            data_dim : int
+                Dimension of data.
+            hidden_dim : int
+                Dimension of hidden layers.
+            augment_dim: int
+                Dimension of augmentation. If 0 does not augment ODE, otherwise augments
+                it with augment_dim dimensions.
+            time_dependent : bool
+                If True adds time as input, making ODE time dependent.
+            non_linearity : string
+                One of 'relu' and 'softplus'
+        """
+        super(ODEFunc, self).__init__(**kwargs)
+        self.augment_dim = augment_dim
+        self.data_dim = data_dim
+        self.input_dim = data_dim + augment_dim
+        self.hidden_dim = hidden_dim
+        self.nfe = 0  # Number of function evaluations
+        self.time_dependent = time_dependent
+
+        self.fc1 = tf.keras.layers.Dense(hidden_dim)
+        self.fc2 = tf.keras.layers.Dense(hidden_dim)
+        self.fc3 = tf.keras.layers.Dense(self.input_dim)
+
+        if non_linearity == 'relu':
+            self.non_linearity = tf.keras.layers.ReLU()
+        elif non_linearity == 'softplus':
+            self.non_linearity = tf.keras.layers.Activation('softplus')
+
+    def call(self, t, x, training=None, **kwargs):
+        """
+        Forward pass. If time dependent, concatenates the time
+        dimension onto the input before the call to the dense layer.
+
+        # Arguments:
+            t: Tensor. Current time. Shape (1,).
+            x: Tensor. Shape (batch_size, input_dim).
+
+        # Returns:
+            Output tensor of forward pass.
+        """
+
+        # Forward pass of model corresponds to one function evaluation, so
+        # increment counter
+        self.nfe += 1
+        if self.time_dependent:
+            # Shape (batch_size, 1)
+            t_vec = tf.ones([x.shape[0], 1]) * t
+            # Shape (batch_size, data_dim + 1)
+            t_and_x = tf.concat([t_vec, x], axis=-1)
+            # Shape (batch_size, hidden_dim)
+            out = self.fc1(t_and_x)
+        else:
+            out = self.fc1(x)
+        out = self.non_linearity(out)
+        out = self.fc2(out)
+        out = self.non_linearity(out)
+        out = self.fc3(out)
+        return out
+
+
+class ODEBlock(tf.keras.Model):
+
+    def __init__(self, odefunc, is_conv=False, tol=1e-3, adjoint=False, solver='dopri5', **kwargs):
+        """
+        Solves ODE defined by odefunc.
+
+        # Arguments:
+            odefunc : ODEFunc instance or ConvODEFunc instance
+                Function defining dynamics of system.
+            is_conv : bool
+                If True, treats odefunc as a convolutional model.
+            tol : float
+                Error tolerance.
+            adjoint : bool
+                If True calculates gradient with adjoint solver, otherwise
+                backpropagates directly through operations of ODE solver.
+            solver: ODE solver. Defaults to DOPRI5.
+        """
+        super(ODEBlock, self).__init__(**kwargs)
+
+        if adjoint:
+            raise NotImplementedError("adjoint solver has not been implemented yet !")
+
+        self.adjoint = adjoint
+        self.is_conv = is_conv
+        self.odefunc = odefunc
+        self.tol = tol
+        self.method = solver
+        self.channel_axis = 1 if tf.keras.backend.image_data_format() == 'channels_first' else -1
+
+    def call(self, x, training=None, eval_times=None, **kwargs):
+        """
+        Solves ODE starting from x.
+
+        # Arguments:
+            x: Tensor. Shape (batch_size, self.odefunc.data_dim)
+            eval_times: None or tf.Tensor.
+                If None, returns solution of ODE at final time t=1. If tf.Tensor
+                then returns full ODE trajectory evaluated at points in eval_times.
+
+        # Returns:
+            Output tensor of forward pass.
+        """
+
+        # Forward pass corresponds to solving ODE, so reset number of function
+        # evaluations counter
+        self.odefunc.nfe = 0
+
+        if eval_times is None:
+            integration_time = tf.convert_to_tensor([0, 1], dtype=x.dtype)
+        else:
+            integration_time = tf.cast(eval_times, x.dtype)
+
+        if self.odefunc.augment_dim > 0:
+            if self.is_conv:
+                # Add augmentation
+                if self.channel_axis == 1:
+                    batch_size, channels, height, width = x.shape
+                    aug = tf.zeros([batch_size, self.odefunc.augment_dim,
+                                    height, width])
+
+                else:
+                    batch_size, height, width, channels = x.shape
+
+                    aug = tf.zeros([batch_size, height, width,
+                                    self.odefunc.augment_dim])
+
+                # Shape (batch_size, channels + augment_dim, height, width)
+                x_aug = tf.concat([x, aug], axis=self.channel_axis)
+            else:
+                # Add augmentation
+                aug = tf.zeros([x.shape[0], self.odefunc.augment_dim])
+                # Shape (batch_size, data_dim + augment_dim)
+                x_aug = tf.concat([x, aug], axis=-1)
+        else:
+            x_aug = x
+
+        if self.adjoint:
+            # TODO: Replace with odeint_adjoint once implemented !
+            out = odeint(self.odefunc, x_aug, integration_time,
+                         rtol=self.tol, atol=self.tol, method=self.method,
+                         options={'max_num_steps': MAX_NUM_STEPS})
+        else:
+            out = odeint(self.odefunc, x_aug, integration_time,
+                         rtol=self.tol, atol=self.tol, method=self.method,
+                         options={'max_num_steps': MAX_NUM_STEPS})
+
+        if eval_times is None:
+            return out[1]  # Return only final time
+        else:
+            return out
+
+    def trajectory(self, x, timesteps):
+        """Returns ODE trajectory.
+        Parameters
+        ----------
+        x : torch.Tensor
+            Shape (batch_size, self.odefunc.data_dim)
+        timesteps : int
+            Number of timesteps in trajectory.
+        """
+        integration_time = tf.linspace(0., 1., timesteps)
+        return self.call(x, eval_times=integration_time)
+
+
+class ODENet(tf.keras.Model):
+
+    def __init__(self, data_dim, hidden_dim, output_dim=1,
+                 augment_dim=0, time_dependent=False, non_linearity='relu',
+                 tol=1e-3, adjoint=False, solver='dopri5', **kwargs):
+        """
+        An ODEBlock followed by a Linear layer.
+
+        # Arguments:
+            data_dim : int
+                Dimension of data.
+            hidden_dim : int
+                Dimension of hidden layers.
+            output_dim : int
+                Dimension of output after hidden layer. Should be 1 for regression or
+                num_classes for classification.
+            augment_dim: int
+                Dimension of augmentation. If 0 does not augment ODE, otherwise augments
+                it with augment_dim dimensions.
+            time_dependent : bool
+                If True adds time as input, making ODE time dependent.
+            non_linearity : string
+                One of 'relu' and 'softplus'
+            tol : float
+                Error tolerance.
+            adjoint : bool
+                If True calculates gradient with adjoint method, otherwise
+                backpropagates directly through operations of ODE solver.
+            solver: ODE solver. Defaults to DOPRI5.
+        """
+        super(ODENet, self).__init__(**kwargs)
+
+        if adjoint:
+            raise NotImplementedError("adjoint solver has not been implemented yet !")
+
+        self.data_dim = data_dim
+        self.hidden_dim = hidden_dim
+        self.augment_dim = augment_dim
+        self.output_dim = output_dim
+        self.time_dependent = time_dependent
+        self.tol = tol
+
+        odefunc = ODEFunc(data_dim, hidden_dim, augment_dim,
+                          time_dependent, non_linearity)
+
+        self.odeblock = ODEBlock(odefunc, tol=tol, adjoint=adjoint, solver=solver)
+        self.linear_layer = tf.keras.layers.Dense(self.output_dim)
+
+    def call(self, x, training=None, return_features=False):
+        features = self.odeblock(x, training=training)
+        pred = self.linear_layer(features)
+        if return_features:
+            return features, pred
+        return pred
