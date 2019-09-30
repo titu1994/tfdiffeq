8cf73b0b2f398508d31b4b5f324fc5b4e6cec71b
Somshubra Majumdar <titu1994@gmail.com>
2019-09-29 21:24:27 -0500

    Fix RK4 fixed step solver issue with dtype casting

--- tfdiffeq/rk_common.py
+++ tfdiffeq/rk_common.py
@@ -84,6 +84,11 @@ def rk4_alt_step_func(func, t, dt, y, k1=None):
     k1 = cast_double(k1)
 
     k2 = func(t + dt / 3, tuple(y_ + dt * k1_ / 3 for y_, k1_ in zip(y, k1)))
+    k2 = cast_double(k2)
+
     k3 = func(t + dt * 2 / 3, tuple(y_ + dt * (k1_ / -3 + k2_) for y_, k1_, k2_ in zip(y, k1, k2)))
+    k3 = cast_double(k3)
+
     k4 = func(t + dt, tuple(y_ + dt * (k1_ - k2_ + k3_) for y_, k1_, k2_, k3_ in zip(y, k1, k2, k3)))
+    k4 = cast_double(k4)
     return tuple((k1_ + 3 * k2_ + 3 * k3_ + k4_) * (dt / 8) for k1_, k2_, k3_, k4_ in zip(k1, k2, k3, k4))
