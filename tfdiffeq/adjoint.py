45daa7fac14c09a1c25a659762fe110425dbae3c
Eshref Yozdemir <eshrefyoz@gmail.com>
2020-02-29 16:37:40 +0100

    Fix adjoint interface to work with tf custom_grad
    tensorflow custom_gradient decorator requires the
    grad function to return the gradients as a pair.
    The first value should contain the gradients of
    all the inputs passed to the function (in our
    case this is x0 and t). The second element must
    contain the gradients for the model parameters
    which are tf.Variable objects stored in the ODE
    object. We don't use these params in the function
    interface; instead tf passes all the trainable
    parameters related to our method in variables
    keyword argument.

--- tfdiffeq/adjoint.py
+++ tfdiffeq/adjoint.py
@@ -34,8 +34,7 @@ def OdeintAdjointMethod(*args):
     rtol = _arguments.rtol
     atol = _arguments.atol
 
-    assert len(args) >= 3, 'Internal error: all arguments required.'
-    y0, t, flat_params = args[:-2], args[-2], args[-1]
+    y0, t = args[:-1], args[-1]
 
     # registers `t` as a Variable that needs a gred, then resets it to a Tensor
     # for the `odeint` function to work. This is done to force tf to allow us to
@@ -48,17 +47,13 @@ def OdeintAdjointMethod(*args):
     @func_cast_double
     def grad(*grad_output, variables=None):
         global _arguments
-        # t, flat_params, *ans = ctx.saved_tensors
-        # ans = tuple(ans)
-        # func, rtol, atol, method, options = ctx.func, ctx.rtol, ctx.atol, ctx.method, ctx.options
+        flat_params = _flatten(variables)
+
         func = _arguments.func
         method = _arguments.method
         options = _arguments.options
         rtol = _arguments.rtol
         atol = _arguments.atol
-
-        print("Gradient Output : ", grad_output)
-        print("Variables : ", variables)
 
         n_tensors = len(ans)
         f_params = tuple(variables)
@@ -70,26 +65,22 @@ def OdeintAdjointMethod(*args):
 
             y, adj_y = y_aug[:n_tensors], y_aug[n_tensors:2 * n_tensors]  # Ignore adj_time and adj_params.
 
-            # t = tf.get_variable('t', initializer=t)
-            # y = tuple(tf.Variable(y_) for y_ in y)
-
             with tf.GradientTape() as tape:
                 tape.watch(t)
                 tape.watch(y)
                 func_eval = func(t, y)
                 func_eval = cast_double(func_eval)
 
-            # print('y', [y_.numpy().shape for y_ in y])
-            # print('adj y', [a.numpy().shape for a in adj_y])
-
-            vjp_t, *vjp_y_and_params = tape.gradient(func_eval, (t,) + y + f_params,
-                                                     # list(-adj_y_ for adj_y_ in adj_y),
-                                                     )
+            gradys = tf.expand_dims(list(-adj_y_ for adj_y_ in adj_y), axis=0)
+            #gradys = tf.reshape(list(-adj_y_ for adj_y_ in adj_y), (1, -1))
+            vjp_t, *vjp_y_and_params = tape.gradient(
+                func_eval,
+                (t,) + y + f_params,
+                output_gradients=gradys
+             )
 
             vjp_y = vjp_y_and_params[:n_tensors]
             vjp_params = vjp_y_and_params[n_tensors:]
-            # print('vjp_y', [v.numpy().shape if v is not None else None for v in vjp_y])
-            # print()
 
             # autograd.grad returns None if no gradient, set to zero.
             vjp_t = tf.zeros_like(t, dtype=t.dtype) if vjp_t is None else vjp_t
@@ -101,12 +92,6 @@ def OdeintAdjointMethod(*args):
             if _check_len(f_params) == 0:
                 vjp_params = tf.convert_to_tensor(0., dtype=vjp_y[0].dype)
                 vjp_params = move_to_device(vjp_params, vjp_y[0].device)
-
-            # print('vjp_t grad', vjp_t.numpy())
-            # print('vjp_params', [v.numpy() for v in vjp_params])
-            # print('vjp y grads', [v.numpy().shape for v in vjp_y])
-            # print("LEN FUNC EVALS : ", len(func_eval))
-            # print()
 
             return (*func_eval, *vjp_y, vjp_t, vjp_params)
 
@@ -152,13 +137,6 @@ def OdeintAdjointMethod(*args):
 
             aug_y0 = (*ans_i, *adj_y, adj_time, adj_params)
 
-            # print('ans i', [a.numpy().shape for a in ans_i])
-            # print('adj y', [a.numpy().shape for a in adj_y])
-            # print('adj time', adj_time.numpy().shape)
-            # print('adj params', adj_params.numpy().shape)
-
-            # print()
-
             aug_ans = odeint(
                 augmented_dynamics,
                 aug_y0,
@@ -182,12 +160,6 @@ def OdeintAdjointMethod(*args):
         time_vjps.append(adj_time)
         time_vjps = tf.concat(time_vjps[::-1], 0)
 
-        print()
-        print('adj y', len(adj_y))
-        print('time vjps', time_vjps.shape)
-        print('adj params', adj_params.shape)
-        print()
-
         # reshape the parameters back into the correct variable shapes
         var_flat_lens = [_numel(v, dtype=tf.int32).numpy() for v in variables]
         var_shapes = [v.shape for v in variables]
@@ -196,27 +168,7 @@ def OdeintAdjointMethod(*args):
         adj_params_list = [tf.reshape(p, v_shape)
                            for p, v_shape in zip(adj_params_splits, var_shapes)]
 
-        # add the time gradient (always the first tensor in list of variables)
-        # adj_params.insert(0, time_vjps)
-
-        # adj_y_grad_vars = list(zip(adj_y, grad_output))
-        # time_grad_vars = list((time_vjps, t))
-        model_vars = list(adj_params_list)  # list(zip(adj_params, variables))
-
-        grad_vars = model_vars  # adj_y_grad_vars + time_grad_vars + model_vars
-        # print('adj y grad', len(adj_y_grad_vars))
-        # print('time grad', len(time_grad_vars))
-        print('model grad', len(model_vars))
-        print('model grad values', [v for v in grad_vars])
-        print()
-
-        # if len(adj_y) == 1:
-        #     adj_y = adj_y[0]
-
-        return (adj_y, model_vars)
-        # print("Grad_vars : ", grad_vars)
-        # return zip(*grad_vars)  # (*adj_y, time_vjps, adj_params, None, None)
-        # return (*adj_y, time_vjps, adj_params)
+        return (*adj_y, time_vjps), adj_params_list
 
     return ans, grad
 
@@ -247,12 +199,10 @@ def odeint_adjoint(func, y0, t, rtol=1e-6, atol=1e-12, method=None, options=None
         if not func.built:
             _ = func(t, y0)
 
-        flat_params = _flatten(func.variables)
-
         global _arguments
         _arguments = _Arguments(func, method, options, rtol, atol)
 
-        ys = OdeintAdjointMethod(*y0, t, flat_params)
+        ys = OdeintAdjointMethod(*y0, t)
 
         if tensor_input or type(ys) == tuple or type(ys) == list:
             ys = ys[0]
