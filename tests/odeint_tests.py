18f2203b963610ae92daf946d6d64b4788b732e7
Eshref Yozdemir <eshrefyoz@gmail.com>
2020-02-29 16:41:57 +0100

    Enable adjoint tests except adams method
    Adams method for some reason crashes. Maybe this
    can be fixed in a later revision

--- tests/odeint_tests.py
+++ tests/odeint_tests.py
@@ -68,13 +68,13 @@ class TestSolverError(unittest.TestCase):
             with self.subTest(ode=ode):
                 self.assertLess(rel_error(sol, y), error_tol)
 
-    # def test_adjoint(self):
-    #     for ode in problems.PROBLEMS.keys():
-    #         f, y0, t_points, sol = problems.construct_problem(TEST_DEVICE, reverse=True)
-    #
-    #         y = tfdiffeq.odeint_adjoint(f, y0, t_points, method='dopri5')
-    #         with self.subTest(ode=ode):
-    #             self.assertLess(rel_error(sol, y), error_tol)
+    def test_adjoint(self):
+        for ode in problems.PROBLEMS.keys():
+            f, y0, t_points, sol = problems.construct_problem(TEST_DEVICE, reverse=True)
+    
+            y = tfdiffeq.odeint_adjoint(f, y0, t_points, method='dopri5')
+            with self.subTest(ode=ode):
+                self.assertLess(rel_error(sol, y), error_tol)
 
 
 class TestSolverBackwardsInTimeError(unittest.TestCase):
@@ -119,13 +119,13 @@ class TestSolverBackwardsInTimeError(unittest.TestCase):
             with self.subTest(ode=ode):
                 self.assertLess(rel_error(sol, y), error_tol)
 
-    # def test_adjoint(self):
-    #     for ode in problems.PROBLEMS.keys():
-    #         f, y0, t_points, sol = problems.construct_problem(TEST_DEVICE, reverse=True)
-    #
-    #         y = tfdiffeq.odeint_adjoint(f, y0, t_points, method='dopri5')
-    #         with self.subTest(ode=ode):
-    #             self.assertLess(rel_error(sol, y), error_tol)
+    def test_adjoint(self):
+        for ode in problems.PROBLEMS.keys():
+            f, y0, t_points, sol = problems.construct_problem(TEST_DEVICE, reverse=True)
+    
+            y = tfdiffeq.odeint_adjoint(f, y0, t_points, method='dopri5')
+            with self.subTest(ode=ode):
+                self.assertLess(rel_error(sol, y), error_tol)
 
 
 class TestNoIntegration(unittest.TestCase):
