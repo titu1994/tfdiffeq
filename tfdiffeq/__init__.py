a10ab632d76b6abced183af84099cad0344d84ea
KuzMenachem <35711942+KuzMenachem@users.noreply.github.com>
2020-07-21 17:22:48 +0200

    Update Conv2DODE and DenseODE models

--- tfdiffeq/__init__.py
+++ tfdiffeq/__init__.py
@@ -3,7 +3,6 @@ from tfdiffeq.odeint import odeint
 from tfdiffeq.adjoint import odeint_adjoint
 
 # Utility functions
-from tfdiffeq.misc import cast_double, func_cast_double
 from tfdiffeq.misc import move_to_device
 
 # Visualization functions
